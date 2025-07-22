a7eb24f9d448bd9a5f023beebad423ad2355ab88
somanath-10 <somanathr1006@gmail.com>
2025-07-22 22:22:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,11 @@ const app = express();
 dotenv.config();
 app.use(express.json({ limit: "30mb", extended: true }))
 app.use(express.urlencoded({ limit: "30mb", extended: true }))
-app.use(cors());
+app.use(cors(
+    {
+          origin: 'https://codequest-iota.vercel.app',
+    }
+));
 app.use(
     fileUpload({ 
         useTempFiles:true,                    
