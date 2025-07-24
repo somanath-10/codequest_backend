2b3bdea4632c5352bc45c1d944dcea4ef8ebcb2e
somanath-10 <somanathr1006@gmail.com>
2025-07-24 13:37:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ import publicChatRoutes from "./routes/PublicChat.js";
 const app = express();
 app.use(cors(
     {
-          origin: 'https://codequest-iota.vercel.app',
+          origin: "*",
             credentials: true,
 
     }
